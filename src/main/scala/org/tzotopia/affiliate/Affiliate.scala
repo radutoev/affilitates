e9993dc3d3b907ba983c7361a2321e8b8462f8b2
Radu Toev <radu.toev@siemens.com>
2019-06-30 23:44:59 +0300

    Escape quotes from header names

--- src/main/scala/org/tzotopia/affiliate/Affiliate.scala
+++ src/main/scala/org/tzotopia/affiliate/Affiliate.scala
@@ -59,7 +59,7 @@ object Affiliate extends IOApp {
 
   def cronInit: IO[Unit] = IO {
     //every 2 seconds
-    val cron = Cron.unsafeParse("0 33 * ? * *") //0 1 * * * * -> every day at 1 am.
+    val cron = Cron.unsafeParse("0 41 23 ? * *")
 
     val scheduled = awakeEveryCron[IO](cron) >> Stream.emits(config.affiliateNames.toSeq)
       .evalMap { name =>
